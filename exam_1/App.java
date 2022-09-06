1ccabfae3ec12a5e301d5c904288fb0e5152562e
Irayamae <111834309+Irayamae@users.noreply.github.com>
2022-09-06 10:13:51 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,18 @@
+public class App{
+    public static void main(String[] args) throws Exception {
+        Student me = new Student();
+        me.firstName = "Fitz Kirktene";
+        me.middleInitial = "None";
+        me.Surname = "Villanueva";
+        me.dateOfBirth = 06/26/03;
+        me.studentNumber = 2022-104420;
+        me.emailAddress = "villanuevaf@students.national-u.edu.ph";
+        me.iAmAwesome = true;
+        me.sayMystudentNumber();
+        me.sayMyEmailAddress();
+        me.AmIAwesome();
+
+    }
+
+
+}
